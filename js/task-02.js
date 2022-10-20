ce5ee3ceb54820bbe7633aae3c519dcdccd66845
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-20 18:45:58 +0300

    done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredient = document.createElement("li");
+//   ingredient.textContent = ingredients[i];
+//   console.log(ingredient);
+// }
+
+const ingredientsRefs = document.querySelector("#ingredients");
+const itemsArr = [];
+
+ingredients.forEach((el) => {
+  const item = document.createElement("li");
+  item.textContent = el;
+  item.classList.add("item");
+  itemsArr.push(item);
+});
+
+ingredientsRefs.append(...itemsArr);
