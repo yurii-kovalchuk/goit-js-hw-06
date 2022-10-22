fbd52c8112fe5350a654c905c759f2d33b6f4a58
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-22 20:24:29 +0300

    done task-04

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+const decBtn = document.querySelector('button[data-action="decrement"]');
+const incBtn = document.querySelector('button[data-action="increment"]');
+const valueRefs = document.querySelector("#value");
+let counterValue = Number(valueRefs.textContent);
+
+decBtn.addEventListener("click", callback);
+incBtn.addEventListener("click", callback);
+
+function callback(evt) {
+  if (evt.currentTarget.dataset.action === "decrement") {
+    counterValue -= 1;
+  } else {
+    counterValue += 1;
+  }
+  valueRefs.textContent = counterValue;
+}
