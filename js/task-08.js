24c046d47b23cafe41dba93bf0956fbe4387d164
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-24 23:33:30 +0300

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRefs = document.querySelector(".login-form");
 
+formRefs.addEventListener("submit", callback);
+
+function callback(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  if (email.value && password.value) {
+    const result = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(result);
+
+    evt.currentTarget.reset();
+    return;
+  }
+  alert("Please fill in all fields!");
+}
