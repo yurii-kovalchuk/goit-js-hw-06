904b427d799e9713fed41fa4c9098a64c8cdcc4f
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-24 20:38:32 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,20 +3,6 @@ const outputRefs = document.querySelector("#name-output");
 
 inputRefs.addEventListener("input", callback);
 
-// 1st method with "inputRefs.value"
-//
-// function callback() {
-//   if (inputRefs.value) {
-//     outputRefs.textContent = inputRefs.value;
-//   } else {
-//     outputRefs.textContent = "Anonymous";
-//   }
-// }
-//
-// чи обов’язково застосовувати "evt.currentTarget.value" замість "inputRefs.value" ?
-
-// 2nd method with "evt.currentTarget.value"
-//
 function callback(evt) {
   if (evt.currentTarget.value) {
     outputRefs.textContent = evt.currentTarget.value;
