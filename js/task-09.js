b56697c717c16e7dacb0da7344e606182e77d098
yurii-kovalchuk <sogdrums@gmail.com>
2022-10-25 13:28:10 +0300

    done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRefs = document.querySelector(".change-color");
+const nameRefs = document.querySelector(".color");
+const bodyRefs = document.querySelector("body");
+
+btnRefs.addEventListener("click", changeColor);
+
+function changeColor() {
+  let clr = getRandomHexColor();
+  bodyRefs.style.backgroundColor = clr;
+  nameRefs.textContent = clr;
+}
